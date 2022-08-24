ba9f70b88d06d921aa80f94993c93546ee48cf39
mecitcanyazar <mecitcanyazar@gmail.com>
2022-08-24 20:32:35 +0300

    weather app commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from "axios";
+import React, { useState } from "react";
 
-function App() {
+const App = () => {
+  const [searchValue, setSearchValue] = useState("");
+  const [data,setData] = useState(null)
+
+  const searchCity = async (event) => {
+    try {
+      if (event.key === "Enter") {
+        const key = "76dc95e283bf9730745671aa1099004c";
+        await axios
+          .get(
+            `https://api.openweathermap.org/data/2.5/weather?q=${searchValue}&APPID=${key}&lang=tr&units=metric`
+          )
+          // .then((res) => console.log(res.data));
+          .then((res) => setData(res.data));
+      }
+    } catch (err) {
+      console.warn(err);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="header">
+        <h1>Hava Durumu</h1>
+        <input
+          type="text"
+          value={searchValue}
+          placeholder="Şehir Giriniz.."
+          onChange={(e ) => setSearchValue(e.target.value)}
+          onKeyPress={searchCity}
+        />
+      </div>
+      <div className="content" >
+        {data && 
+          <div>
+            <div className="city" id="city">{data.name}</div>
+            <div className="temp">{data.main.temp}°C</div>
+            <div className="desc">{data.weather[0].description.toUpperCase()}</div>
+            <div className="minmax">Min : {data.main.temp_min}°C /  Max: {data.main.temp_max} °C</div>
+          </div>
+        }
+      </div>
     </div>
   );
-}
+};
 
 export default App;
